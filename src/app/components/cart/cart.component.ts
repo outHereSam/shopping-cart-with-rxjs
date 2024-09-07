f8f0d0b885073d435ee5f3db2c9705394ecfc435
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-07 15:00:24 +0000

    Calculates total cart price

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CartItem } from '../../interfaces/ICartItems';
 import { AsyncPipe } from '@angular/common';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [AsyncPipe, CartItemComponent],
+  imports: [AsyncPipe, CurrencyPipe, CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.sass',
 })
@@ -18,5 +19,18 @@ export class CartComponent {
   constructor(protected cartService: CartService) {
     this.cartItems$ = cartService.getCartItems();
     this.cartQuantity$ = cartService.cartQuantity$;
+  }
+
+  getTotalPrice(): Observable<number> {
+    return this.cartItems$.pipe(
+      map((cartItems) => {
+        return cartItems.reduce((total, item) => {
+          const cartItem = cartItems.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          return total + (cartItem?.price || 0) * item.quantity;
+        }, 0);
+      })
+    );
   }
 }
